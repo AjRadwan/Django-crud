1c7a914910c835baa9207564caaf53100b5b3311
Anowar <1hafizajr@gmail.com>
2022-01-04 22:46:16 +0600

    include model and get data from database in view

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from core.models import Student
 # Create your Viewss here.
 
 class Home(View):
     def get(self, request):
-        return render(request, 'home.html')
+        student = Student.objects.all()
+        return render(request, 'home.html', {'students':student})
     
  
